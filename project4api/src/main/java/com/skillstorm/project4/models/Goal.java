c67c44bf9b28ada0d52e4ce807ab3c10b50bdb1b
joshua <joshu@DESKTOP-LN7DOPJ>
2022-08-26 23:31:50 -0400

    project initialization

--- project4api/src/main/java/com/skillstorm/project4/models/Goal.java
+++ project4api/src/main/java/com/skillstorm/project4/models/Goal.java
@@ -37,4 +37,121 @@ public class Goal {
 	@OneToMany(mappedBy = "goal")
 	private Set<Transaction> transaction;
 
+	public Goal() {
+	}
+
+	public Goal(String name, String startDate, String endDate, Account account, long targetGoal, long currentAmount) {
+		this.name = name;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.account = account;
+		this.targetGoal = targetGoal;
+		this.currentAmount = currentAmount;
+	}
+
+	public Goal(int id, String name, String startDate, String endDate, Account account, long targetGoal,
+			long currentAmount, Set<Transaction> transaction) {
+		this.id = id;
+		this.name = name;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.account = account;
+		this.targetGoal = targetGoal;
+		this.currentAmount = currentAmount;
+		this.transaction = transaction;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	public long getTargetGoal() {
+		return targetGoal;
+	}
+
+	public void setTargetGoal(long targetGoal) {
+		this.targetGoal = targetGoal;
+	}
+
+	public long getCurrentAmount() {
+		return currentAmount;
+	}
+
+	public void setCurrentAmount(long currentAmount) {
+		this.currentAmount = currentAmount;
+	}
+
+	public Set<Transaction> getTransaction() {
+		return transaction;
+	}
+
+	public void setTransaction(Set<Transaction> transaction) {
+		this.transaction = transaction;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Goal other = (Goal) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Goal [account=" + account + ", currentAmount=" + currentAmount + ", endDate=" + endDate + ", id=" + id
+				+ ", name=" + name + ", startDate=" + startDate + ", targetGoal=" + targetGoal + ", transaction="
+				+ transaction + "]";
+	}
+
 }
